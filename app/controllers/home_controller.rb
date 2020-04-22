c5679f3b1ead45ae2bec5af641d8e967042b830e
Ritesh Kapoor <ritesh@workoidz.com>
2020-04-22 14:58:12 +0530

    Search Option for Channel as well Show is Added at Home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def show
+  	p " in show method: " 
+
   	@shows = Show.all
+
+  	if params[:search_show] != nil then @shows = Show.where(show_name: params[:search_show]) end #p " Show : " + params[:search_show] end
+
+  	if params[:search_channel] != nil then @shows = Channel.find_by(channel_name: params[:search_channel]).shows  end #p " channel : " + params[:search_channel] end
+
   end
 end
